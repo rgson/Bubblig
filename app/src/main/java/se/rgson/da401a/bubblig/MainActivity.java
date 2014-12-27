aaed993b0488067e5ff3489acf18ea4551457b3e
Robin Gustafsson <robingustafsson93@gmail.com>
2014-12-27 15:41:18 +0100

    Fixed proper drawer/action bar interaction

--- app/src/main/java/se/rgson/da401a/bubblig/MainActivity.java
+++ app/src/main/java/se/rgson/da401a/bubblig/MainActivity.java
@@ -2,8 +2,14 @@ package se.rgson.da401a.bubblig;
 
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import se.rgson.da401a.bubblig.gui.ArticleFragment;
 import se.rgson.da401a.bubblig.gui.ArticleListFragment;
@@ -16,8 +22,12 @@ public class MainActivity extends Activity implements CategoryListFragment.Categ
 
 	private static String TAG = MainActivity.class.getSimpleName();
 
-	private boolean tabletLayout = false;
+	private boolean mTabletLayout = false;
+	private float mDrawerOffset = 0.0f;
+	private String mTitle;
+
 	private DrawerLayout mDrawerLayout;
+	private ActionBarDrawerToggle mDrawerToggle;
 	private CategoryListFragment mCategoryListFragment;
 	private ArticleListFragment mArticleListFragment;
 	private ArticleFragment mArticleFragment;
@@ -28,8 +38,13 @@ public class MainActivity extends Activity implements CategoryListFragment.Categ
 		setContentView(R.layout.activity_main);
 
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
 
-		tabletLayout = (findViewById(R.id.article_container) != null);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+
+		mTabletLayout = (findViewById(R.id.article_container) != null);
+		mTitle = getResources().getString(R.string.app_name);
 
 		if (savedInstanceState == null) {
 
@@ -37,7 +52,7 @@ public class MainActivity extends Activity implements CategoryListFragment.Categ
 			mArticleListFragment = ArticleListFragment.newInstance();
 			mArticleFragment = ArticleFragment.newInstance();
 
-			if (!tabletLayout) {
+			if (!mTabletLayout) {
 				getFragmentManager().beginTransaction()
 						.add(R.id.drawer_container, mCategoryListFragment)
 						.add(R.id.container, mArticleListFragment)
@@ -52,11 +67,69 @@ public class MainActivity extends Activity implements CategoryListFragment.Categ
 
 		}
 
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(mTitle);
+				invalidateOptionsMenu();
+			}
+
+			public void onDrawerOpened(View view) {
+				getActionBar().setTitle(getResources().getString(R.string.app_name));
+				invalidateOptionsMenu();
+			}
+
+			@Override
+			public void onDrawerSlide(View drawerView, float slideOffset) {
+				super.onDrawerSlide(drawerView, slideOffset);
+				mDrawerOffset = slideOffset;
+				invalidateOptionsMenu();
+			}
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		int alpha = (int) (255 * (1 - mDrawerOffset));
+		for (int i = 0; i < menu.size(); i++) {
+			MenuItem item = menu.getItem(i);
+			if (item.getIcon() != null) {
+				item.getIcon().setAlpha(alpha);
+			}
+			item.setVisible(mDrawerOffset < 1.0f);
+		}
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+		super.setTitle(title);
+		mTitle = (String) title;
 	}
 
 	@Override
 	public void onCategorySelected(Category category) {
-		if (!tabletLayout) {
+		if (!mTabletLayout) {
 			getFragmentManager().beginTransaction()
 					.replace(R.id.container, mArticleListFragment)
 					.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
@@ -69,7 +142,7 @@ public class MainActivity extends Activity implements CategoryListFragment.Categ
 
 	@Override
 	public void onArticleSelected(Article article) {
-		if (!tabletLayout) {
+		if (!mTabletLayout) {
 			getFragmentManager().beginTransaction()
 					.replace(R.id.container, mArticleFragment)
 					.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
